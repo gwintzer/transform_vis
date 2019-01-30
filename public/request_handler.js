e987d866a8d7693aa87e3936ff597d80a97e0af1
ghfan <ghfan@TestDASH.com>
2019-01-30 13:50:56 +0800

    import plotly.js

--- public/request_handler.js
+++ public/request_handler.js
@@ -1,12 +1,12 @@
 import chrome from 'ui/chrome';
 import { dashboardContextProvider } from 'plugins/kibana/dashboard/dashboard_context'
-
 const Mustache = require('mustache');
+const Plotly = require('plotly.js');
 
 export const createRequestHandler = function(Private, es, indexPatterns, $sanitize) {
-  
+
     const myRequestHandler = (vis, state) => {
-  
+
       var { appState, uiState, query, filters, timeRange, searchSource } = state;
 
       /* As per https://github.com/elastic/kibana/issues/17722 dashboard_context will go away soon.
@@ -15,31 +15,31 @@ export const createRequestHandler = function(Private, es, indexPatterns, $saniti
 
       const dashboardContext = Private(dashboardContextProvider);
       const options = chrome.getInjected('transformVisOptions');
-      
+
       return new Promise((resolve, reject) => {
-        
+
         function display_error(message) {
           resolve({ html: `<div style="text-align: center;"><i>${message}</i></div>`});
         }
 
         function search(indexPattern){
-                  
+
           // This is part of what should be a wider config validation
           if (indexPattern === undefined || indexPattern.id === undefined) {
             display_error("No Index Pattern");
             return;
-          } 
-    
+          }
+
           const context = dashboardContext();
-          
+
           if (indexPattern.timeFieldName) {
             const timefilterdsl = {range: {}};
             timefilterdsl.range[indexPattern.timeFieldName] = {gte: timeRange.from, lte: timeRange.to};
             context.bool.must.push(timefilterdsl);
           }
-      
+
           const body = JSON.parse(vis.params.querydsl.replace('"_DASHBOARD_CONTEXT_"', JSON.stringify(context)));
-      
+
           // Can be used for testing the above commented future change //
           // console.log("context", JSON.stringify(context));
           // console.log("searchSource", searchSource);
@@ -52,7 +52,7 @@ export const createRequestHandler = function(Private, es, indexPatterns, $saniti
             index: indexPattern.title,
             body: body
           }, function (error, response) {
-    
+
             if (error) {
               display_error("Error (See Console)");
               console.log("Elasticsearch Query Error", error);
@@ -73,31 +73,31 @@ export const createRequestHandler = function(Private, es, indexPatterns, $saniti
                   return; // Abort!
                 }
                 if (typeof bindme.meta.before_render === "function") { bindme.meta.before_render(); }
-                resolve({ 
-                  html: Mustache.render(formula, bindme), 
+                resolve({
+                  html: Mustache.render(formula, bindme),
                   after_render: bindme.meta.after_render
                 });
-              
+
               } else {
                 resolve({ html: $sanitize(Mustache.render(formula, bindme)) });
               }
-              
+
             }
-            
+
           });
-    
+
         }
-        
+
         indexPatterns.get(vis.params.indexpattern).then(function (indexPattern) {
           search(indexPattern);
         });
-        
+
       });
-  
+
     };
-  
+
     return myRequestHandler;
-    
-  } 
- 
-  
+
+  }
+
+
