3a73324a5b0bbea0d39eaed5934e7220a6fee3bf
Guillaume Wintzer <guillaume.wintzer@orange.com>
2019-10-04 09:49:04 +0200

    start from the v7.3 update from guanghaofan. fix issues on build with styles import

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
+import { existsSync } from 'fs';
 
     require: ['elasticsearch'],
     name: 'transform_vis',
+    publicDir: resolve(__dirname, 'public'),
     uiExports: {
       visTypes: [
         'plugins/transform_vis/transform_vis'
@@ -10,7 +12,7 @@ import { resolve } from 'path';
           transformVisOptions: options
       }),
       },
-      styleSheetPaths: resolve(__dirname, 'public/index.scss'),
+      styleSheetPaths: [resolve(__dirname, 'public/index.scss'), resolve(__dirname, 'public/index.css')].find(p => existsSync(p)),
     },
     config: (Joi) => Joi.object({
         enabled: Joi.boolean().default(true),
